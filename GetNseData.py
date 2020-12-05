142abf7484945a8aec69fb943e71363b1a323690
jsmith1309 <jsmith130940@gmail.com>
2020-12-06 01:58:34 +0530

    Update GetNseData.py

--- GetNseData.py
+++ GetNseData.py
@@ -5,22 +5,25 @@ class GetNseData:
     def data(self, symbol, date1, date2):
         path = "/home/pi/Documents/py/{}.csv".format(symbol)
         stock_csv(symbol, from_date=date1, to_date=date2, series="EQ", output=path)
-    
-list1 = ["SBIN", "ACC"]
 
-date_entry = input('Enter a start date in DD-MM-YYYY format: ')
-day, month, year = map(int, date_entry.split('-'))
-date1 = datetime.date(year, month, day)
+def get_dates():
+    date_entry = input('Enter a start date in DD-MM-YYYY format: ')
+    day, month, year = map(int, date_entry.split('-'))
+    date1 = datetime.date(year, month, day)
+    date_entry = input('Enter a end date in DD-MM-YYYY format: ')
+    day, month, year = map(int, date_entry.split('-'))
+    date2 = datetime.date(year, month, day)
+    return date1, date2
 
-date_entry = input('Enter a end date in DD-MM-YYYY format: ')
-day, month, year = map(int, date_entry.split('-'))
-date2 = datetime.date(year, month, day)
+def get_symbols():
+    file = open("./xyz.txt")
+    lines = file.read().splitlines()
+    file.close()
 
-for i in list1:
+symbols = get_symbols()
+start_date, end_date = get_dates()
+
+for i in symbols:
     stock = i
     i = GetNseData()
-    i.data(stock, date1, date2)
-    
-
-
-
+    i.data(stock, start_date, end_date)
